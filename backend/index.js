6bb488cc0aade00c1a7d5e26353d62b1dbd1841a
AlejandroVelasHel <velalejandro23@gmail.com>
2024-05-24 15:30:30 -0500

    eliminación de base de datos

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,6 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 
 import routerContacto from './routes/routesContacto.js'
-import routerProducto from './routes/routesProducto.js'
-import routerPagos from './routes/routesPago.js'
 
 
 const app = express()
@@ -16,24 +14,7 @@ const PORT = process.env.PORT || 3000
 
 
 app.use('/api/v1', routerContacto)
-app.use('/api/v1', routerProducto)
-app.use('/api/v1', routerPagos)
 
-
-
-// Configurar CORS con las opciones personalizadas
-// const dominiosPermitidos = ['http://localhost:5173','http://localhost:4000']; // Agrega aquí los dominios permitidos
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (dominiosPermitidos.indexOf(origin) !== -1) {
-//       // Origin permitido
-//       callback(null, true);
-//     } else {
-//       callback(new Error('No permitido por CORS'));
-//     }
-//   }
-// };
-// app.use(cors(corsOptions));
 
 app.get('/', (req, res)=>{
     res.send("hola")
--- backend/routes/routesContacto.js
+++ backend/routes/routesContacto.js
@@ -1,17 +1,11 @@
 import express from 'express'
 import { handleContactForm } from '../controllers/controllerContacto.js';
-import { 
-        getContacto, 
-        addContacto,
-        getContactoOne,
-    } from '../controllers/controllerContacto.js';
+
 
 
 const router = express.Router();
 
-router.get('/contactos', getContacto)
-router.get('/contactos/:id', getContactoOne)
-router.post('/contactos', addContacto)
+
 router.post('/contact', handleContactForm)
 
 
